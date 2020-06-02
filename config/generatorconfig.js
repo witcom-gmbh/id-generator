8c0ce988d82483d75c20aeebe834f7c4ac47f204
Carsten Buchberger <c.buchberger@witcom.de>
2020-06-02 15:18:01 +0000

    Real external config ;-)

--- config/generatorconfig.js
+++ config/generatorconfig.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); // this loads the defined variables from .env
 const fs = require('fs');
 
-const idGeneratorConfig = JSON.parse(fs.readFileSync('./config/generatorconfig.json', 'utf-8'));
+const idGeneratorConfig = JSON.parse(fs.readFileSync('./config-external/generatorconfig.json', 'utf-8'));
 
 module.exports = idGeneratorConfig;
