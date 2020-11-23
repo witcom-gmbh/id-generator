c4cfd64d302a656974a84ceead6d1e5f19a40696
Carsten Buchberger <c.buchberger@witcom.de>
2020-11-23 09:32:16 +0000

    Much better healtcheck

--- app/redis.js
+++ app/redis.js
@@ -8,7 +8,7 @@ var settings = {
 
 
 const asyncRedis = require("async-redis");
-var redisClient = asyncRedis.createClient(parseInt(settings.port), settings.host, {connect_timeout: 30000} );
+var redisClient = asyncRedis.createClient(parseInt(settings.port), settings.host, {connect_timeout: 3000} );
 redisClient.auth(settings.password);
 
 var infolog = function (msg) {
@@ -23,20 +23,18 @@ var warnlog = function (msg) {
 };
 var errorlog = function (msg) {
     return function() {
-        //console.log(arguments[0]);
         const redisLogger = logger.child(arguments[0]);
-        //const redisLogger = logger.child({ a: 'property' })
         redisLogger.error(msg);
-        process.exit();
-        
     }
 };
+
+
 
 var url = 'redis://' + redisClient.address;
 redisClient.on('connect'     , infolog('Redis is connecting to ' + url));
 redisClient.on('ready'       , infolog('Redis is ready'));
 redisClient.on('reconnecting', warnlog('Redis is reconnecting to ' + url));
-redisClient.on('error'       , errorlog('Redis error happens'));
+redisClient.on('error'       , errorlog('Redis error'));
 redisClient.on('end'         , infolog('Redis is ended'));
 
 module.exports = redisClient;
